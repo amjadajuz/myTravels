0f6b913088cd028c2743ac18ce61e9748ff2cab3
amjadajuz <ptamjadaju@gmail.com>
2025-07-19 11:40:15 +0530

    Add about and contact pages with navigation and hero sections; implement search functionality in index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+
+function searchDestinations(query) {
+
+    const resultsContainer = document.getElementById('results');
+    resultsContainer.innerHTML = ''; // Clear previous results
+    const data=fetch("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMSkillsNetwork-JS0101EN-SkillsNetwork/travel1.json");
+    data.then(response => response.json())
+    .then(data => {
+       if(query.includes("beaches")) {
+            const beaches = data.beaches.map(beach => {
+                return `<div class="result-item">
+                            <h3>${beach.name}</h3>
+                            <p>${beach.description}</p>
+                        </div>`;
+            }).join('');
+            resultsContainer.innerHTML = beaches;
+        }
+        else if(query.includes("temples")) {
+            const temples = data.temples.map(temple => {
+                return `<div class="result-item">
+                            <h3>${temple.name}</h3>
+                            <p>${temple.description}</p>
+                        </div>`;
+            }).join('');
+            resultsContainer.innerHTML = temples;
+        }
+        else if(query.includes("countries")) {
+            const countries = data.countries.map(country => {
+                return `<div class="result-item">
+                            <h3>${country.name}</h3>
+                            <p>${country.description}</p>
+                        </div>`;
+            }).join('');
+            resultsContainer.innerHTML = countries;
+        }
+    })
+}
+
+document.getElementById('search').addEventListener('input', function() {
+    const query = this.value.toLowerCase();
+    searchDestinations(query)
+});
+
+document.getElementById('searchBtn').addEventListener('click', function() {
+    const query = document.getElementById('search').value.toLowerCase();
+    searchDestinations(query);
+});
+
+document.getElementById('clearBtn').addEventListener('click', function() {
+    document.getElementById('search').value = '';
+    document.getElementById('results').innerHTML = ''; // Clear results
+});
